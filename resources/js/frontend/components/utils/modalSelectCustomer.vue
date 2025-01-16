90135bd90b8da974ffc0d9e178a310a2f1a9c694
frimaSkn <frimasatria347@gmail.com>
2025-01-16 11:04:06 +0700

    update z-index

--- resources/js/frontend/components/utils/modalSelectCustomer.vue
+++ resources/js/frontend/components/utils/modalSelectCustomer.vue
@@ -5,6 +5,10 @@
     const selected = ref("");
     const open = ref(false);
 
+    const form = ref({
+        search: ''
+    });
+
     const options = ref([
         { label: 'Option #1', value: 'option_1' }
     ]);
@@ -32,7 +36,7 @@
     <Transition>
         <dialog v-if="open"
             @click.self="open = false"
-            class="fixed inset-0 z-10 flex items-start justify-center w-full h-full bg-black bg-opacity-25 backdrop-blur-sm"
+            class="fixed inset-0 z-[100] flex items-start justify-center w-full h-full bg-black bg-opacity-25 backdrop-blur-sm"
         >
             <div class="w-full max-w-2xl p-6 mt-20 bg-white rounded-2xl">
                 <VueSelect
@@ -43,6 +47,19 @@
                     @option-deselected="(option) => console.log(option.label, option.value)"
                     placeholder="Select an option"
                 />
+
+                <!-- <div class="relative">
+                    <div class="absolute inset-y-0 flex items-center pointer-events-none start-0 ps-3">
+                        <svg class="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
+                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"/>
+                        </svg>
+                    </div>
+                    <input type="search" class="block w-full p-4 text-sm text-gray-900 border rounded-lg border-primary-50 ps-10 bg-gray-50" placeholder="Search Customers..." required
+                        v-model="form.search"
+                    />
+                </div> -->
+
+                <!-- <div></div> -->
             </div>
         </dialog>
     </Transition>
