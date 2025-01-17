202784d5d15ecc192c9878c864f8e23c4a8cd2e6
Dean Abner Julian <dabnerjulian@yahoo.com>
2025-01-18 01:22:47 +0700

    Refactor Accurate API scopes by removing unused permissions

--- config/accurate.php
+++ config/accurate.php
@@ -12,24 +12,9 @@ return [
 
         'scopes' => [
             'warehouse_view',
-            'customer_save',
-            'customer_view',
-            'customer_delete',
             'item_save',
             'item_view',
-            'item_adjustment_save',
-            'sales_order_save',
-            'glaccount_view',
-            'glaccount_save',
-            'sales_invoice_save',
-            'sales_invoice_view',
-            'sales_receipt_save',
-            'sales_receipt_view',
-            'purchase_invoice_view',
-            'receive_item_view',
-            'stock_mutation_history_view',
-            'department_view',
-            'department_save'
+            'sales_order_save'
         ]
     ]
 ]
