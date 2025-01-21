68854b7f230f4d3b457ceb92c661da967923c050
frimaSkn <frimasatria347@gmail.com>
2025-01-21 11:58:47 +0700

    update

--- resources/js/frontend/components/print/SemiCustom.vue
+++ resources/js/frontend/components/print/SemiCustom.vue
@@ -28,7 +28,7 @@
 </script>
 
 <template>
-    <div class="printable">
+    <div class="printable w-[1400px]">
         <CustomBasic @additionalBasic="additionalBasic" :dataConfig="props.data_config" :dataSemiCustom="props.data_semi_custom" ref="childBasic" />
         <CustomOptions @additionalOption="additionalOption" :dataConfig="props.data_config" :dataSemiCustom="props.data_semi_custom" ref="childOption"/>
     </div>
