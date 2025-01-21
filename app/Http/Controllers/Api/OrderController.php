3122662fc62a2c5deeca2a8f9fdcf685c879d94c
frimaSkn <frimasatria347@gmail.com>
2025-01-22 01:48:22 +0700

    update calulate total price semi custom

--- app/Http/Controllers/Api/OrderController.php
+++ app/Http/Controllers/Api/OrderController.php
@@ -300,7 +300,7 @@ class OrderController extends Controller
                 }
 
                 if ($item->isSemiCustom()) {
-                    $calucate = $item->product->base_price + $item->product->option_total;
+                    $calucate = ($item->product->base_price + $item->product->option_total) - $semiCustomProuduct->option_discount;;
                     $totalPrice += $calucate;
                 }
             }
