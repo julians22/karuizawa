4aec7ee8e651b78bf4b5de6ea0e6dc6f44b8eb5d
Dean Abner Julian <dabnerjulian@yahoo.com>
2025-01-23 16:18:51 +0700

    Add bulk action for syncing orders to Accurate and update configuration for transaction numbers

--- app/Http/Livewire/Backend/OrderTable.php
+++ app/Http/Livewire/Backend/OrderTable.php
@@ -14,6 +14,10 @@ class OrderTable extends DataTableComponent
     public function configure(): void
     {
         $this->setPrimaryKey('id');
+
+        $this->setBulkActions([
+            'syncAccurate' => 'Sync to Accurate',
+        ]);
     }
 
     public function filters(): array
