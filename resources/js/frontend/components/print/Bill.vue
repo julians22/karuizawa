592dd334382981ef64e830e76b2baa286bcbc478
Dean Abner Julian <dabnerjulian@yahoo.com>
2025-01-23 21:15:41 +0700

    Refactor Bill component layout for improved print formatting and readability

--- resources/js/frontend/components/print/Bill.vue
+++ resources/js/frontend/components/print/Bill.vue
@@ -41,17 +41,16 @@ const { handlePrint } = useVueToPrint({
 </script>
 
 <template>
-    <div class="max-w-md p-4 mx-auto">
-        <button @click="handlePrint" class="inline-block p-2 mx-auto text-white bg-primary-50">Print</button>
-        <div ref="componentRef" class="max-w-md border">
+    <div class="mx-auto max-w-[80mm]">
+        <div ref="componentRef" class="px-2 border w-full font-sans print:font-sans" style="font-size: 10px;">
           <div>
-              <div class="text-lg font-bold text-center">KARUIZAWA SHIRT</div>
-              <div class="text-sm font-bold text-center">{{ props.data_order.store.address }}</div>
-              <div class="text-center">Karuizawashirt.official</div>
-              <div class="mt-2 text-center">Casier: {{ props.data_order.user.name }}</div>
+              <div class="font-semibold text-center">KARUIZAWA SHIRT</div>
+              <div class="font-semibold text-center leading-snug">{{ props.data_order.store.address }}</div>
+              <div class="text-center">@karuizawashirt.official</div>
+              <div class="text-center">Casier: {{ props.data_order.user.name }}</div>
               <div class="text-center">{{ orderDate }}</div>
-              <div class="w-full h-px bg-black"></div>
-              <table class="w-full px-2">
+              <div class="bg-black w-full h-px"></div>
+              <table class="w-full">
 
                 <tbody>
                     <template v-for="(order, index) in props.data_order.order_items">
@@ -70,7 +69,7 @@ const { handlePrint } = useVueToPrint({
                         <td class="text-right">{{ priceFormat(subTotal) }}</td>
                     </tr>
 
-                    <tr>
+                    <tr v-if="props.data_order.discount > 0">
                         <td colspan="2" class="text-right">Disc ({{ props.data_order.discount_details.coupon }} %):</td>
                         <td class="text-right">{{ priceFormat(props.data_order.discount) }}</td>
                     </tr>
@@ -82,20 +81,20 @@ const { handlePrint } = useVueToPrint({
                 </tbody>
               </table>
 
-              <div class="w-full h-px bg-black"></div>
+              <div class="bg-black w-full h-px"></div>
 
-                <div class="px-2 py-2">
-                    Terms & Conditions:
-                    <div class="mb-2 text-xs">
-                        <strong class="mb-1 text-base">Semi-Respoke</strong>
+                <div class="py-2">
+                    <strong>Terms & Conditions:</strong>
+                    <div class="mb-2">
+                        <strong class="mb-1">Semi-Respoke</strong>
                         <ol class="list-decimal list-inside">
                             <li>The production process will require 14 working days, excluding holidays and weekends.</li>
                             <li>Karuizawa will charge a revision fee for any changes or modifications depending on the nature and complexity.</li>
                             <li>No refund policy.</li>
                         </ol>
                     </div>
-                    <div class="text-xs">
-                        <strong class="mb-1 text-base">Ready to Wear</strong>
+                    <div class="">
+                        <strong class="mb-1">Ready to Wear</strong>
                         <ol class="list-decimal list-inside">
                             <li>Karuizawa allows any product exchange only for the cause of production's mishaps.</li>
                             <li>Karuizawa needs to receive the receipt, product with tag and label intact during the exchange process within 3 days after purchase.</li>
@@ -104,8 +103,10 @@ const { handlePrint } = useVueToPrint({
                 </div>
 
                 <!-- border dot -->
-                <div class="w-full h-px border-t border-black border-dotted"></div>
+                <div class="border-t border-black border-dotted w-full h-px"></div>
           </div>
         </div>
+
+        <button @click="handlePrint" class="block print:hidden bg-primary-50 mx-auto mb-2 p-2 text-white">Print</button>
     </div>
 </template>
