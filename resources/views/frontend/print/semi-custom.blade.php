013fa1f54bfa37892ddfb7b15f48e0493831d330
frimaSkn <frimasatria347@gmail.com>
2025-01-25 12:24:59 +0700

    update font size and name file save pdf

--- resources/views/frontend/print/semi-custom.blade.php
+++ resources/views/frontend/print/semi-custom.blade.php
@@ -1,10 +1,11 @@
+{{-- @dd($dataSemiCustom) --}}
 <!doctype html>
 <html lang="{{ htmlLang() }}" @langrtl dir="rtl" @endlangrtl>
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    <title>{{ appName() }} @yield('title')</title>
+    <title>{{ $dataSemiCustom['name'] }} - {{ $dataSemiCustom['order_item']['order']['order_number'] }}</title>
     <meta name="description" content="@yield('meta_description', appName())">
     <meta name="author" content="@yield('meta_author', 'F_Skn')">
     @yield('meta')
